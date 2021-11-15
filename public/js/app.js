89eec7f84d76d998dbd55e4771791fde9f4a8032
Michael <mikl@mymts.net>
2021-11-15 14:41:56 -0600

    app is working but still ugly

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,28 @@
 console.log('client side js file loaded!');
+
+
+const weatherForm = document.querySelector('form');
+const search = document.querySelector('input');
+const msgOne = document.querySelector('#msg1');
+const msgTwo = document.querySelector('#msg2');
+
+
+
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    msgOne.textContent = "Loading...";
+    msgTwo.textContent = " ";
+    fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          msgOne.textContent = data.error;
+        } else {
+          msgOne.textContent = data.location;
+          msgTwo.textContent = data.forecast;
+        }
+      });
+    });
+    
+})
