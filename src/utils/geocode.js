0186e885d0c0872f1fc453cc3ec7c3286911a5fb
Michael <mikl@mymts.net>
2021-11-18 17:05:43 -0600

    test edit for heroku

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -9,8 +9,6 @@ const geocode = (address, callback) => {
       
     if (error) {
       callback("Unable to connect to location services", undefined);
-    } else if (body.features.length === 0) {
-      callback("No such location!", undefined);
     } else {
       callback(undefined, {
         latitude: body.features[0].center[1],
